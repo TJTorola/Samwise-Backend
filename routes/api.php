f614ec1c30585a6451e9f698982ac4f2d0043d27
Tyler Torola <tyler@tjt.codes>
2017-06-06 08:13:28 -0700

    Start adding jsonApi

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+JsonApi::api('default', ['as' => 'api.'], function ($api, $router) {
+    $api->resource('users');
 });
