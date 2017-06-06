2e6e7238afd1e5af479e5947a1ef21341db859cb
Tyler Torola <tyler@tjt.codes>
2017-06-06 07:04:22 -0700

    Add a basic seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,32 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Offer::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'type' => 'auto',
+        'type_info' => [
+            'sI' => $faker->boolean,
+            'sII' => $faker->boolean,
+            'sIIA' => $faker->boolean,
+            'l_sIIA' => $faker->boolean,
+            'sIIB' => $faker->boolean,
+            'sIII' => $faker->boolean,
+            'stage_one_v8' => $faker->boolean,
+            'defender' => $faker->boolean,
+            'range_rover' => $faker->boolean,
+            'discovery' => $faker->boolean,
+            'other_applications' => '',
+            'base_80' => $faker->boolean,
+            'base_86' => $faker->boolean,
+            'base_88' => $faker->boolean,
+            'base_107' => $faker->boolean,
+            'base_109' => $faker->boolean,
+        ],
+        'public' => $faker->boolean(95),
+        'description' => $faker->text($faker->biasedNumberBetween(200, 2000)),
+        'long_description' => '',
+        'tags' => '',
+    ];
+});
